cd0ce60df5a360a5860f60399c74c450741333af
Mateusz kwiatkowski <mateusz@serveraptor.com>
2018-10-19 11:34:01 +0200

    Add more commands to webhook

---
+++ start-machine.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+error(){
+  echo "{\"status\": \"error\"}"
+}
+
+trap error ERR;
+
+if [ -v $1 ] ; then
+  echo "Usage: $0 <name>" > /dev/stderr
+  echo "Example: $0 FreeBSD-VM" > /dev/stderr
+  exit 2
+fi
+
+name="$1"
+
+pushd /home/kwiat/vm-bhyve > /dev/null
+./vm start "$name" > /dev/null 2>&1 &
+popd > /dev/null
+
+echo "{\"status\": \"starting\"}"
