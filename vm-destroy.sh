8f02c78072e3d44b402b398fcfe31dd25eda26a9
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-10-24 07:36:25 +0200

    Add endpoint for destroying virtual machines

---
+++ vm-destroy.sh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/bash
+. commons.sh
+
+if [ -v $1 ]; then
+  echo "Usage: $0 <name>" > /dev/stderr
+  echo "Example: $0 FreeBSD-VM" > /dev/stderr
+  exit 2
+fi
+
+name="$1"
+
+pushd /opt/runhyve/vm-bhyve > /dev/null
+./vm destroy -f "$name" > /dev/null 2>&1 &
+popd > /dev/null
+
+echo "{\"status\": \"deleting\"}"
