b95fe16eb85aed43ce8aafd288178a00a4ba982e
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-10-24 13:18:23 +0200

    Add sanity checks in vm-create.sh

--- vm-create.sh
+++ vm-create.sh
@@ -13,11 +13,34 @@ name="$3"
 image="$4"
 network="${5:-public}"
 
+if [ ! -r "/zroot/vm/.templates/${system}-${plan}.conf" ]; then
+  echo "Error: Coulnd't find template for plan ${system}-${plan}"
+  exit 2
+fi
+
+if [ ! -r "/zroot/vm/.img/${image}" ]; then
+  echo "Error: Couldn't find imge ${image}"
+  exit 2
+fi
+
 pushd /opt/runhyve/vm-bhyve > /dev/null
+
+if [ ! -z "$(./vm list | awk "\$1 = /$name/ { print }")" ]; then
+  echo "Error: Virtual machine $name already exist"
+  exit
+fi
+
+if ! ./vm switch list | cut -f1 | grep "$network"; then
+  echo "Error: Network ${network} doesn't exist"
+  exit 2
+fi
+
 ./vm create -t "$system-$plan" -i "$image" "$name" > /dev/null 2>&1 &
+
 if [ "$network" != "public" ]; then
   sysrc -f "/zroot/vm/${name}/${name}.conf" network0_switch="$network"
 fi
+
 popd > /dev/null
 
 echo "{\"status\": \"creating\"}"
