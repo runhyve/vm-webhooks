9d7d3f8f89f05d94d775219cdf1ed8d3cc2b881c
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-10-24 08:22:08 +0200

    Fix attaching vm to default network 'public'

--- vm-create.sh
+++ vm-create.sh
@@ -15,7 +15,7 @@ network="${5:-public}"
 
 pushd /opt/runhyve/vm-bhyve > /dev/null
 ./vm create -t "$system-$plan" -i "$image" "$name" > /dev/null 2>&1 &
-if [ "$network" != "default" ]; then
+if [ "$network" != "public" ]; then
   sysrc -f "/zroot/vm/${name}/${name}.conf" network0_switch="$network"
 fi
 popd > /dev/null
