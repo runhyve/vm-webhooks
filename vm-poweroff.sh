c3f66e11453ccc503ef992656749db2c4e27b47c
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-10-29 09:54:57 +0100

    Add endpoint for hard power off of virtual machine

---
+++ vm-poweroff.sh
@@ -0,0 +1,26 @@
+#!/usr/local/bin/bash
+
+. commons.sh
+
+if [ -v $1 ] ; then
+  echo "Usage: $0 <name>" > /dev/stderr
+  echo "Example: $0 FreeBSD-VM" > /dev/stderr
+  exit 2
+fi
+
+name="$1"
+
+if ! check_vm "$name"; then
+  report_error "Virtual machine ${name} doesn't exist"
+fi
+
+pushd /opt/runhyve/vm-bhyve > /dev/null
+message="$(./vm -f poweroff "$name" 2>&1)"
+
+if [ $? -ne 0 ]; then
+  report_error "$message"
+else
+  report_success "$message"
+fi
+
+popd > /dev/null
