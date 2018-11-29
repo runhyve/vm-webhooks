c2428d77cf6aaa3b0b7af69b5d1432caf956fdce
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-11-29 10:42:21 +0100

    Rename vm-create argument: plan -> template

--- _vm-create.sh
+++ _vm-create.sh
@@ -3,7 +3,11 @@ set -x
 . /opt/runhyve/vm-webhooks/commons.sh
 
 pushd /opt/runhyve/vm-bhyve
-./vm create -t "$_template" -c "$cpu" -m "$memory" -i "$image" -s "$disk" "$name"
+./vm create -t "$template" -c "$cpu" -m "$memory" -i "$image" -s "$disk" "$name"
+
+if [ "$?" -ne 0 ]; then
+  report_error
+fi
 
 if [ "$network" != "public" ]; then
   sysrc -f "/zroot/vm/${name}/${name}.conf" network0_switch="$network"
--- vm-create.sh
+++ vm-create.sh
@@ -2,13 +2,13 @@
 . commons.sh
 
 if [ -v $1 ] || [ -v $2 ] || [ -v $3 ] || [ -v $4 ] || [ -v $5 ] || [ -v $6 ] || [ -v $7 ]; then
-  echo "Usage: $0 <system> <plan> <name> <image> [network]" > /dev/stderr
+  echo "Usage: $0 <system> <template> <name> <image> <cpu> <memory> <disk> [network]" > /dev/stderr
   echo "Example: $0 freebsd 1C-1GB-50HDD FreeBSD-VM FreeBSD-11.2-RELEASE-amd64.raw" > /dev/stderr
   exit 2
 fi
 
 system="$1"
-plan="$2"
+template="$2"
 name="$3"
 image="$4"
 cpu="$5"
@@ -16,12 +16,10 @@ memory="$6"
 disk="$7"
 network="${8:-public}"
 
-_template="${system}-${plan}"
+export system template name image cpu memory network disk _template
 
-export system plan name image cpu memory network disk _template
-
-if ! check_template "$_template"; then
-  report_error "Coulnd't find template for plan ${_template}"
+if ! check_template "$template"; then
+  report_error "Coulnd't find template ${template}"
 fi
 
 if ! check_img "$image"; then
