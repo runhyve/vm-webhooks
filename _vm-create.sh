f04d366d8b4c045c55773537861542a44fc8502e
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2019-04-12 20:44:21 +0200

    Add support for SSH public keys

--- _vm-create.sh
+++ _vm-create.sh
@@ -3,7 +3,18 @@ set -x
 . /opt/runhyve/vm-webhooks/commons.sh
 
 pushd /opt/runhyve/vm-bhyve
-./vm create -t "$template" -c "$cpu" -m "$memory" -i "$image" -s "$disk" "$name"
+
+sshpk="/tmp/.sshpk.$$"
+
+OPTS=" -t $template -c $cpu -m $memory -i $image -s $disk -C "
+
+if [ ! -z "$ssh_public_key" ]; then
+  echo "$ssh_public_key" > "$sshpk"
+  OPTS+=" -k $sshpk "
+fi
+
+./vm create $OPTS "$name"
+rm -f "$sshpk"
 
 if [ "$?" -ne 0 ]; then
   report_error
--- vm-create.sh
+++ vm-create.sh
@@ -2,7 +2,7 @@
 . commons.sh
 
 if [ -v $1 ] || [ -v $2 ] || [ -v $3 ] || [ -v $4 ] || [ -v $5 ] || [ -v $6 ] || [ -v $7 ]; then
-  echo "Usage: $0 <template> <name> <image> <cpu> <memory> <disk> [network]" > /dev/stderr
+  echo "Usage: $0 <template> <name> <image> <cpu> <memory> <disk> [network] [ssh public key]" > /dev/stderr
   echo "Example: $0 freebsd FreeBSD-VM FreeBSD-11.2-RELEASE-amd64.raw" > /dev/stderr
   exit 2
 fi
@@ -14,8 +14,9 @@ cpu="$4"
 memory="$5"
 disk="$6"
 network="${7:-public}"
+ssh_public_key="${8:-}"
 
-export template name image cpu memory network disk _template
+export template name image cpu memory network disk ssh_public_key _template
 
 if ! check_template "$template"; then
   report_error "Coulnd't find template ${template}"
