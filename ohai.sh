a6dd7abea9ee47f11667995a5474935420ecfd0a
Piotr Baj <piotr.baj@petabajt.net>
2021-06-29 13:18:04 +0200

    Search ohai executable in $PATH

--- ohai.sh
+++ ohai.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 . commons.sh
 
-if [ -x /usr/bin/ohai ]; then
+if [ -x $(which ohai) ]; then
   report_success "$(/usr/bin/env ohai -l fatal | sed 's/%"/"/g')"
 else
   report_error "Ohai is not available"
